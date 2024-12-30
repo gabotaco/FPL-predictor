8acaea158416d4d482dfc66265f8e6ff1ed7e6ba
Gabe <42919897+gabotaco@users.noreply.github.com>
2024-12-30 18:43:34 -0500

    Simulate predicting 38 week ahead

--- simulate.py
+++ simulate.py
@@ -13,10 +13,10 @@ from main import make_predictions, get_predict_by
 DATA_YEAR = "2023-24"
 DATA_WEEK_RANGE = (19, 38)
 MAKE_PREDICTIONS = True
-PREDICTION_TAG = "cal10/pred1"
+PREDICTION_TAG = "cal10/pred20"
 
 MAX_DIFF = 10
-PREDICT_BY_WEEKS = 1
+PREDICT_BY_WEEKS = 20
 CALIBRATE_BY = 10
 USE_AVERAGE = False
 
