1c44b4df6aea6f654aaa263728359cec1c2cf1c1
Gabe <42919897+gabotaco@users.noreply.github.com>
2025-08-16 11:37:25 -0400

    Simulate early/noAverage/predictMaxCalibrateGw: 450 points

--- simulate.py
+++ simulate.py
@@ -12,7 +12,7 @@ from main import make_predictions, get_predict_by, MIN_CALIBRATE_BY
 DATA_YEAR = "2024-25"
 DATA_WEEK_RANGE = (2, 10)
 MAKE_PREDICTIONS = True
-PREDICTION_TAG = "early/noAverage/predictMaxCalibrate38"
+PREDICTION_TAG = "early/noAverage/predictMaxCalibrateGw"
 
 MAX_DIFF = 10
 MIN_GAMES = 3
@@ -22,7 +22,7 @@ USE_AVERAGE = False
 
 
 def get_calibrate_by(gw):
-    return SEASON_LENGTH
+    return gw
 
 
 def get_predict_by_weeks(gw):
