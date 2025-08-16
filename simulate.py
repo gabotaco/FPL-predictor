0219de08e66a8f150e5474df3c4dcd20b112cca1
Gabe <42919897+gabotaco@users.noreply.github.com>
2025-08-16 14:56:54 -0400

    Simulate first10/average/minGames1: 483 points

--- simulate.py
+++ simulate.py
@@ -12,10 +12,10 @@ from main import make_predictions, get_predict_by, MIN_CALIBRATE_BY
 DATA_YEAR = "2024-25"
 DATA_WEEK_RANGE = (2, 10)
 MAKE_PREDICTIONS = True
-PREDICTION_TAG = "first10/average/minGames3"
+PREDICTION_TAG = "first10/average/minGames1"
 
 MAX_DIFF = 10
-MIN_GAMES = 3
+MIN_GAMES = 1
 MIN_SEASON_PPG = 1
 MIN_SEASON_GAME_PERCENTAGE = 0.8
 USE_AVERAGE = True
